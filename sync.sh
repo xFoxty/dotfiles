5c587bfb59d923a5b69188a611bd6d20bf160233
贺俊浩 <nougat_hx@qq.com>
2026-01-20 14:46:07 +0800

    Update configs: 2026-01-20 14:46:07

--- sync.sh
+++ sync.sh
@@ -4,11 +4,12 @@ TARGET_DIR=$(pwd)
 
 mkdir -p "$TARGET_DIR/nvim"
 mkdir -p "$TARGET_DIR/yazi"
+mkdir -p "$TARGET_DIR/wezterm"
 
 cp ~/.vimrc ~/dotfiles/
 rsync -av --delete --exclude="lazy-lock.json" "$HOME/.config/nvim/" "$TARGET_DIR/nvim/"
 rsync -av --delete "$HOME/.config/yazi/" "$TARGET_DIR/yazi/"
-
+rsync -av --delete "$HOME/.config/wezterm" "$TARGET_DIR/wezterm"
 git add .
 git commit -m "Update configs: $(date +'%Y-%m-%d %H:%M:%S')"
 git push origin main
